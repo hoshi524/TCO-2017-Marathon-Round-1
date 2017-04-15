9133a412ed22cef0f22567420f60708875786c16
hoshi524 <hoshi524@gmail.com>
2017-04-15 16:07:14 +0900

    fix

--- src/GraphDrawing.cpp
+++ src/GraphDrawing.cpp
@@ -7,11 +7,11 @@ const int max_vertex = 1000;
 bool used[max_size][max_size];
 int N;
 int E;
-int sum_value;
+double sum_value;
 int vertices[max_vertex][2];
 int edges[max_vertex][max_vertex];
 int length[max_vertex][max_vertex];
-int vertex_value[max_vertex];
+double vertex_value[max_vertex];
 int center_dist[max_vertex];
 
 double get_time() {
@@ -25,54 +25,40 @@ unsigned get_random() {
   return y ^= (y ^= (y ^= y << 13) >> 17) << 5;
 }
 
-int calc_dist(int i, int j, int x, int y) {
+double calc_dist(int i, int j, int x, int y) {
   int a = i - x;
   int b = j - y;
   return sqrt((double)(a * a + b * b));
 }
 
-int calc_center_dist(int i, int j) {
+double calc_center_dist(int i, int j) {
   return calc_dist(max_size / 2, max_size / 2, i, j);
 }
 
-int calc_dist(int i, int j) {
+double calc_dist(int i, int j) {
   return calc_dist(vertices[i][0], vertices[i][1], vertices[j][0],
                    vertices[j][1]);
 }
 
-int calc_value(int i, int j) {
+double calc_value(int i, int j) {
   const int d = calc_dist(i, j);
   const int len = length[i][j];
   assert(d > 0);
-  assert(len > 0);
-  int ratio;
+  double ratio;
   if (d > len) {
-    ratio = (d << 4) / len;
+    ratio = (double)d / len;
   } else {
-    ratio = (len << 4) / d;
+    ratio = (double)len / d;
   }
-  return ratio * ratio;
+  return ratio * ratio * ratio * ratio;
 }
 
-int calc_score(int x) {
-  assert(x < N);
-  assert(edges[x][0] < N);
-  int sum = 0;
+double calc_score(int x) {
+  double sum = 0;
   for (int i = 1, size = edges[x][0]; i <= size; ++i) {
     sum += calc_value(x, edges[x][i]);
   }
-  assert(sum >= 0);
   return sum;
-}
-
-int select_vertex() {
-  assert(sum_value >= 0);
-  int v = get_random() % sum_value;
-  for (int i = 0; i < N; ++i) {
-    if (v < vertex_value[i]) return i;
-    v -= vertex_value[i];
-  }
-  return -1;
 }
 
 class GraphDrawing {
@@ -106,25 +92,36 @@ class GraphDrawing {
     const int batch = (1 << 12) - 1;
     int iterate = 0;
     while (true) {
-      const double time = (get_time() - START_TIME);
-      if (time > TIME_LIMIT) break;
+      const double time = (START_TIME + TIME_LIMIT - get_time()) / TIME_LIMIT;
+      if (time < 0) break;
       ++iterate;
       for (; (iterate & batch) != batch; ++iterate) {
         const int v = get_random() % N;
-        // const int v = select_vertex();
         assert(v != -1);
         const int pr = vertices[v][0];
         const int pc = vertices[v][1];
         int row, col;
-        while (true) {
-          row = get_random() % max_size;
-          col = get_random() % max_size;
-          if (!used[row][col]) break;
+        {
+          int dist = max_size * time / 2;
+          if (dist < 5) dist = 5;
+          int a = pr - dist;
+          if (a < 0) a = 0;
+          int b = pc - dist;
+          if (b < 0) b = 0;
+          int c = pr + dist;
+          if (c > max_size) c = max_size;
+          int d = pc + dist;
+          if (d > max_size) d = max_size;
+          while (true) {
+            row = a + get_random() % (c - a);
+            col = b + get_random() % (d - b);
+            if (!used[row][col]) break;
+          }
         }
         vertices[v][0] = row;
         vertices[v][1] = col;
-        int ns = calc_score(v);
-        int allow = vertex_value[v] * (TIME_LIMIT - time) / TIME_LIMIT / 2;
+        double ns = calc_score(v);
+        double allow = vertex_value[v] * time / 2;
         if (vertex_value[v] > ns - allow) {
           sum_value += (ns - vertex_value[v]) * 2;
           vertex_value[v] = ns;
--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ mkdir out
 
 javac src/GraphDrawingVis.java -d out
 
-g++ src/GraphDrawing.cpp
+g++ --std=c++0x -W -Wall -Wno-sign-compare -O2 -s -pipe -mmmx -msse -msse2 -msse3 src/GraphDrawing.cpp
 mv a.out out/
 
 java -cp out/ GraphDrawingVis -exec out/a.out
