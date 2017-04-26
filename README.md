f3b09eea67efd0b9f2f00106b212f58e01f64719
hoshi524 <hoshi524@gmail.com>
2017-04-26 21:45:39 +0900

    fix

--- README.md
+++ README.md
@@ -7,17 +7,3 @@ https://community.topcoder.com/longcontest/?module=ViewStandings&rd=16903
 
 ## memo
 
-近傍の選択について改善したい？
-
-## algorithm
-
-- ランダムに頂点を設定
-- 近傍定義
-    - 1点のみ動かす方が良いか、多点が良いか
-    - 遷移先は、上下か、斜めも含めるか
-- 重心を中央に直す
-
-## memo
-
-testcase = 2000
-current  = 0.7900
--- src/GraphDrawing.cpp
+++ src/GraphDrawing.cpp
@@ -6,7 +6,14 @@ constexpr double TIME_LIMIT = 9800;
 constexpr int max_edge = 64;
 constexpr int max_size = 700;
 constexpr int max_vertex = 1000;
+
+struct Vertex {
+  int id;
+  double v;
+};
+
 int N;
+Vertex sort_vertex[max_vertex];
 int esize[max_vertex];
 int edges[max_vertex][max_edge][2];
 int vertex[max_vertex][2];
@@ -42,45 +49,49 @@ double calc_score(int x, int r, int c) {
   return max;
 }
 
-bool apply1(int x, int r, int c, int a, int b, double time) {
-  int s1 = 0, s2 = 0;
+tuple<int, int> apply1(int x, int r, int c, int a, int b) {
+  int v1 = 0, v2 = 0;
   for (int i = 0; i < esize[x]; ++i) {
     const int y = edges[x][i][0];
     const int l = edges[x][i][1];
-    s1 += abs(calc_dist(r, c, vertex[y][0], vertex[y][1]) - l);
-    s2 += abs(calc_dist(a, b, vertex[y][0], vertex[y][1]) - l);
+    v1 += abs(calc_dist(r, c, vertex[y][0], vertex[y][1]) - l);
+    v2 += abs(calc_dist(a, b, vertex[y][0], vertex[y][1]) - l);
   }
-  return s1 * (1 + get_random_double() * time * 0.7) > s2;
+  return make_tuple(v1, v2);
 }
 
-bool apply2(int x, int r, int c, int a, int b, double time) {
-  double m1 = 1, m2 = 1;
+tuple<int, int> apply2(int x, int r, int c, int a, int b) {
+  int v1 = 0, v2 = 0;
   for (int i = 0; i < esize[x]; ++i) {
     const int y = edges[x][i][0];
     const int l = edges[x][i][1];
     {
-      const int d = calc_dist(r, c, vertex[y][0], vertex[y][1]);
-      const double r = d > l ? (double)d / l : (double)l / d;
-      if (m1 < r) m1 = r;
+      int d = calc_dist(r, c, vertex[y][0], vertex[y][1]);
+      if (d == 0) d = 1;
+      const int r = d > l ? (d << 10) / l : (l << 10) / d;
+      if (v1 < r) v1 = r;
     }
     {
-      const int d = calc_dist(a, b, vertex[y][0], vertex[y][1]);
-      const double r = d > l ? (double)d / l : (double)l / d;
-      if (m2 < r) m2 = r;
+      int d = calc_dist(a, b, vertex[y][0], vertex[y][1]);
+      if (d == 0) d = 1;
+      const int r = d > l ? (d << 10) / l : (l << 10) / d;
+      if (v2 < r) v2 = r;
     }
   }
-  m1 -= 1;
-  m2 -= 1;
-  return m1 * (1 + get_random_double() * time * 0.7) > m2;
+  v1 -= 1 << 10;
+  v2 -= 1 << 10;
+  return make_tuple(v1, v2);
 }
 
 void annealing(double end,
-               bool (*apply)(int x, int r, int c, int a, int b, double time)) {
+               tuple<int, int> (*apply)(int x, int r, int c, int a, int b)) {
   while (true) {
     const double time = (START_TIME + TIME_LIMIT - get_time()) / TIME_LIMIT;
     if (time < end) break;
     const int md = 1 + 400 * time;
-    for (int v = 0; v < N; ++v) {
+    for (int i = 0; i < (1 << 10); ++i) {
+      const int x = (pow(get_random_double(), 2.0) - 0.0001) * N;
+      const int v = sort_vertex[x].id;
       const int pr = vertex[v][0];
       const int pc = vertex[v][1];
       int row, col;
@@ -89,17 +100,23 @@ void annealing(double end,
         const int maxr = min(pr + md, max_size) + 1;
         const int minc = max(pc - md, 0);
         const int maxc = min(pc + md, max_size) + 1;
-        while (true) {
+        do {
           row = minr + get_random() % (maxr - minr);
           col = minc + get_random() % (maxc - minc);
-          if (pr != row || pc != col) break;
-        }
+        } while (pr == row && pc == col);
       }
-      if (apply(v, pr, pc, row, col, time)) {
+      int v1, v2;
+      tie(v1, v2) = apply(v, pr, pc, row, col);
+      if (v1 * (1 + get_random_double() * time * 0.7) > v2) {
+        sort_vertex[x].v = v2;
         vertex[v][0] = row;
         vertex[v][1] = col;
+      } else {
+        sort_vertex[x].v = v1;
       }
     }
+    sort(sort_vertex, sort_vertex + N,
+         [](const Vertex& a, const Vertex& b) { return a.v > b.v; });
   }
 }
 
@@ -110,14 +127,14 @@ class GraphDrawing {
     N = N_;
     for (int i = 0; i <= max_size; ++i) {
       for (int j = 0; j <= max_size; ++j) {
-        DIST[i][j] = sqrt((double)(i * i + j * j)) * (1 << 15);
+        DIST[i][j] = sqrt((double)(i * i + j * j)) * (1 << 10);
       }
     }
     memset(esize, 0, sizeof(esize));
     for (int i = 0, size = edges_.size() / 3; i < size; ++i) {
       const int v1 = edges_[i * 3 + 0];
       const int v2 = edges_[i * 3 + 1];
-      const int len = edges_[i * 3 + 2] << 15;
+      const int len = edges_[i * 3 + 2] << 10;
       edges[v1][esize[v1]][0] = v2;
       edges[v1][esize[v1]][1] = len;
       edges[v2][esize[v2]][0] = v1;
@@ -129,14 +146,11 @@ class GraphDrawing {
       assert(esize[i] < max_edge);
       vertex[i][0] = get_random() % max_size;
       vertex[i][1] = get_random() % max_size;
+      sort_vertex[i] = (Vertex){i, 0};
     }
     annealing(0.2, apply1);
     annealing(0.0, apply2);
     {
-      struct Vertex {
-        int id;
-        double v;
-      };
       vector<Vertex> vv;
       for (int i = 0; i < N; ++i) {
         vv.push_back((Vertex){i, calc_score(i, vertex[i][0], vertex[i][1])});
